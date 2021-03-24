ef0d755294c462f27e30e7ff2b74822bc3cf938c
Julianb18 <julian141718@gmail.com>
2021-03-24 14:09:22 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="grid-container">
+      <header className="row">
+        <div>
+          <a className="brand" href="index.html">
+            LOGO
+          </a>
+        </div>
+        <div>
+          <a href="cart.html">Cart</a>
+          <a href="signin.html">Sign In</a>
+        </div>
       </header>
+      <main>
+        <div>
+          <div className="row center">
+            <div className="card">
+              <a href="product.html">
+                {/* <div
+                  className="img-container"
+                  style="
+                    background-image: url(https://www.buyabroadweb.com/assets/images/71nHh1FKYrL._AC_SL1500_.jpg);
+                  "
+                ></div> */}
+                <img
+                  className="medium"
+                  src="./images/razer.jpg"
+                  alt="product"
+                />
+              </a>
+              <div className="card-body">
+                <a href="product.html">
+                  <h2>Razer Viper Ultimate</h2>
+                </a>
+                <div className="rating">
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                </div>
+                <div className="price">€120</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+      <footer className="row center">All right reserved</footer>
     </div>
   );
 }
